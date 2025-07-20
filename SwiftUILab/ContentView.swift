3f8eb165e00fb8541d79a39aa84649b198de42b5
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-07-20 17:08:32 +0900

    Initial Commit

---
+++ SwiftUILab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUILab
+//
+//  Created by Masahiko Kobayashi on 2025/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUILab/SwiftUILabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILabApp.swift
+//  SwiftUILab
+//
+//  Created by Masahiko Kobayashi on 2025/07/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
